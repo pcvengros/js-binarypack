fb6a45e4fc2b1998334d21a4c6b3db0d2c0f29e0
Pavol Cvengros <pavol.cvengros@securcom.me>
2019-07-26 11:11:25 +0200

    fix grunt, v0.1.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,52 @@
-module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    browserify: {
-      dev: {
-        options: {
-          prepend: ['/*! binarypack.js build:<%= pkg.version %>, development. '+
-                    'Copyright(c) 2012 Eric Zhang <eric@ericzhang.com> MIT Licensed */']
+        browserify: {
+            dev: {
+                options: {
+                    prepend: ['/*! binarypack.js build:<%= pkg.version %>. ' +
+                    'Copyright(c) 2019 Pavol Cvengros <pavol.cvengros@securcom.me>, 2012-2019 Eric Zhang <eric@ericzhang.com> MIT Licensed */']
+                },
+                src: ['lib/exports.js'],
+                dest: 'dist/binarypack.js'
+            }
         },
-        src: ['lib/exports.js'],
-        dest: 'dist/binarypack.js'
-      }
-    },
 
-    uglify: {
-      prod: {
-        options: {
-          mangle: true, compress: true,
-                  },
-        src: 'dist/binarypack.js',
-        dest: 'dist/binarypack.min.js'
-      }
-    },
-
-    concat: {
-      dev: {
-        options: {
-          banner: '/*! binarypack.js build:<%= pkg.version %>, production. '+
-            'Copyright(c) 2012 Eric Zhang <eric@ericzhang.com> MIT Licensed */'
+        uglify: {
+            prod: {
+                options: {
+                    mangle: true,
+                    compress: true,
+                },
+                src: 'dist/binarypack.js',
+                dest: 'dist/binarypack.min.js'
+            }
         },
-        src: 'dist/binarypack.js',
-        dest: 'dist/binarypack.js',
-      },
-      prod: {
-        options: {
-          banner: '/*! binarypack.js build:<%= pkg.version %>, production. '+
-            'Copyright(c) 2012 Eric Zhang <eric@ericzhang.com> MIT Licensed */'
-        },
-        src: 'dist/binarypack.min.js',
-        dest: 'dist/binarypack.min.js',
-      }
-    }
-  });
 
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+        concat: {
+            dev: {
+                options: {
+                    banner: '/*! binarypack.js build:<%= pkg.version %>. ' +
+                        'Copyright(c) 2019 Pavol Cvengros <pavol.cvengros@securcom.me>, 2012-2019 Eric Zhang <eric@ericzhang.com> MIT Licensed */'
+                },
+                src: 'dist/binarypack.js',
+                dest: 'dist/binarypack.js',
+            },
+            prod: {
+                options: {
+                    banner: '/*! binarypack.js build:<%= pkg.version %>. ' +
+                        'Copyright(c) 2019 Pavol Cvengros <pavol.cvengros@securcom.me>, 2012-2019 Eric Zhang <eric@ericzhang.com> MIT Licensed */'
+                },
+                src: 'dist/binarypack.min.js',
+                dest: 'dist/binarypack.min.js',
+            }
+        }
+    });
 
-  grunt.registerTask('default', ['browserify', 'uglify', 'concat']);
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('default', ['browserify', 'uglify', 'concat']);
 };
